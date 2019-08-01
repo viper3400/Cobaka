29c2dca3168d8a3b2eeee838277336c9588fee29
viper3400 <viper_3400@gmx.de>
2019-08-01 16:03:21 +0200

    Add app icon

--- Jaxx.Net.Cobaka/ViewModels/MainWindowViewModel.cs
+++ Jaxx.Net.Cobaka/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace Jaxx.Net.Cobaka.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Cobaka";
         public string Title
         {
             get { return _title; }
