56e852221f0b2f840206834cd8e86deac1e67ba6
viper3400 <viper_3400@gmx.de>
2019-08-01 11:57:04 +0200

    Implemented settings panel in UI

--- Jaxx.Net.Cobaka.NoiseDetector/ViewModels/NoiseDetectorViewModel.cs
+++ Jaxx.Net.Cobaka.NoiseDetector/ViewModels/NoiseDetectorViewModel.cs
@@ -11,11 +11,17 @@ namespace Jaxx.Net.Cobaka.NoiseDetector.ViewModels
 {
     public class NoiseDetectorViewModel : BindableBase
     {
-        private readonly NAudioHandler _audio;        
-
+        private readonly NAudioHandler _audio;
+        private INoiseDetectorOptions _noiseDetectorOptions;
         public NoiseDetectorViewModel(INoiseDetectorOptions options)
         {
-            _audio = new NAudioHandler(options);
+            _noiseDetectorOptions = options;
+            // inital fill settings with default value
+            RecordTreshold = _noiseDetectorOptions.Treshold;
+            DestinationDirectory = _noiseDetectorOptions.DestinationDirectory;
+            DurationInSeconds = (int)_noiseDetectorOptions.RecordDuration.TotalSeconds;
+
+            _audio = new NAudioHandler(_noiseDetectorOptions);
             _audio.RecordStarted += Audio_RecordStarted;
             _audio.RecordStopped += Audio_RecordStopped;
             _audio.SampleAvailable += Audio_SampleAvailable;
@@ -44,10 +50,43 @@ namespace Jaxx.Net.Cobaka.NoiseDetector.ViewModels
             get { return _message; }
             set { SetProperty(ref _message, value); }
         }
-        
+
+        private double _recordTreshold;
+        public double RecordTreshold
+        {
+            get { return _recordTreshold; }
+            set
+            {                
+                SetProperty(ref _recordTreshold, value);
+                _noiseDetectorOptions.Treshold = _recordTreshold;
+            }
+        }
+
+        private int _durationInSeconds;
+        public int DurationInSeconds
+        {
+            get { return _durationInSeconds; }
+            set
+            {
+                SetProperty(ref _durationInSeconds, value);
+                _noiseDetectorOptions.RecordDuration = new TimeSpan(0, 0, _durationInSeconds);
+            }
+        }
+
+        private string _destinationDirectory;
+        public string DestinationDirectory
+        {
+            get { return _destinationDirectory; }
+            set
+            {
+                SetProperty(ref _destinationDirectory, value);
+                _noiseDetectorOptions.DestinationDirectory = _destinationDirectory;
+            }
+        }
+
         public float PeakValue
         {
-            get { return (float)Math.Round(_audio.PeakValue * 100,2); }    
+            get { return (float)Math.Round(_audio.PeakValue * 100, 2); }
         }
 
         private IEnumerable<string> deviceList;
@@ -56,7 +95,7 @@ namespace Jaxx.Net.Cobaka.NoiseDetector.ViewModels
             get { return deviceList; }
             set { SetProperty(ref deviceList, value); }
         }
-        
+
         private DelegateCommand _deviceList;
         public DelegateCommand GetDeviceList =>
             _deviceList ?? (_deviceList = new DelegateCommand(ExecuteGetDeviceList, CanExecuteGetDeviceList));
@@ -93,13 +132,13 @@ namespace Jaxx.Net.Cobaka.NoiseDetector.ViewModels
 
         void ExecuteStopRecord()
         {
-            _audio.StopRecord();            
+            _audio.StopRecord();
         }
 
         bool CanExecuteStopRecord()
         {
             return _audio.IsRecording;
         }
-        
+
     }
 }
