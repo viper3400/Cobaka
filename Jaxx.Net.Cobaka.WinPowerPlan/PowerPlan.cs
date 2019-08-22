40fbd2e1ec1b544d518cd21f3005ed5a88e56d5d
viper_3400 <viper3400@gmx.de>
2019-08-22 14:44:23 +0200

    Implemented base classes

---
+++ Jaxx.Net.Cobaka.WinPowerPlan/PowerPlan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Jaxx.Net.Cobaka.WinPowerPlan
+{
+    public class PowerPlan
+    {
+        public static void SetActive(Guid powerSchemeId)
+        {
+            var schemeGuid = powerSchemeId;
+            PowrProf.PowerSetActiveScheme(IntPtr.Zero, ref schemeGuid);
+        }
+
+        public static IEnumerable<Guid> FindAll()
+        {
+            var schemeGuid = Guid.Empty;
+            uint sizeSchemeGuid = (uint)Marshal.SizeOf(typeof(Guid));
+            uint schemeIndex = 0;
+            while (PowrProf.PowerEnumerate(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, (uint)PowrProf.AccessFlags.ACCESS_SCHEME, schemeIndex, ref schemeGuid, ref sizeSchemeGuid) == 0)
+            {
+                yield return schemeGuid;
+                schemeIndex++;
+            }
+        }
+
+        public static string ReadFriendlyName(Guid schemeGuid)
+        {
+            uint sizeName = 1024;
+            IntPtr pSizeName = Marshal.AllocHGlobal((int)sizeName);
+            string friendlyName;
+            try
+            {
+                PowrProf.PowerReadFriendlyName(IntPtr.Zero, ref schemeGuid, IntPtr.Zero, IntPtr.Zero, pSizeName, ref sizeName);
+                friendlyName = Marshal.PtrToStringUni(pSizeName);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(pSizeName);
+            }
+            return friendlyName;
+        }
+
+        public static Guid GetActive()
+        {
+            IntPtr pCurrentSchemeGuid = IntPtr.Zero;
+            PowrProf.PowerGetActiveScheme(IntPtr.Zero, ref pCurrentSchemeGuid);
+            var currentSchemeGuid = (Guid)Marshal.PtrToStructure(pCurrentSchemeGuid, typeof(Guid));
+            return currentSchemeGuid;
+        }
+    }
+}
---
+++ Jaxx.Net.Cobaka.WinPowerPlan/PowrProf.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Jaxx.Net.Cobaka.WinPowerPlan
+{
+    public static class PowrProf
+    {
+        [DllImport("PowrProf.dll")]
+        public static extern UInt32 PowerReadFriendlyName(IntPtr RootPowerKey, ref Guid SchemeGuid, IntPtr SubGroupOfPowerSettingGuid, IntPtr PowerSettingGuid, IntPtr Buffer, ref UInt32 BufferSize);
+
+        [DllImport("PowrProf.dll")]
+        public static extern UInt32 PowerEnumerate(IntPtr RootPowerKey, IntPtr SchemeGuid, IntPtr SubGroupOfPowerSettingGuid, UInt32 AcessFlags, UInt32 Index, ref Guid Buffer, ref UInt32 BufferSize);
+
+        [DllImport("PowrProf.dll")]
+        public static extern uint PowerGetActiveScheme(IntPtr UserRootPowerKey, ref IntPtr ActivePolicyGuid);
+
+        [DllImport("PowrProf.dll")]
+        public static extern uint PowerSetActiveScheme(IntPtr UserRootPowerKey, ref Guid SchemeGuid);
+
+        public enum AccessFlags : uint
+        {
+            ACCESS_SCHEME = 16,
+            ACCESS_SUBGROUP = 17,
+            ACCESS_INDIVIDUAL_SETTING = 18
+        }
+    }
+}
